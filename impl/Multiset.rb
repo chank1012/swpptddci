5eab2112c265d91cd10c713fb23941cdcfb52a08
prox <chank1012@naver.com>
2015-05-04 20:15:04 +0900

    multiset 1

--- impl/Multiset.rb
+++ impl/Multiset.rb
@@ -1,1 +1,28 @@
+class Multiset
+  @@hash
 
+  def initialize(n)
+    @@hash = Hash.new(0) #default:0
+    @@hash[n] = 1
+  end
+
+  def contains(n)
+    if @@hash[n] > 0
+      return true
+    else
+      return false
+    end
+  end
+
+  def add(n)
+    @@hash[n] += 1
+  end
+
+  def remove(n)
+    if @@hash[n] == 0
+      raise
+    else
+      @@hash[n] -= 1
+    end
+  end
+end
